2598f655c69a00d44c0abaaca9b480c6e7e1a0c8
BlakeZajac <contact@blakezajac.com>
2022-10-23 11:45:23 +1100

    Small changes to styles

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
-console.log(`
-                Designed & Developed by Blake Zajac
-                  https://github.com/BlakeZajac/`);
+console.log(
+  `Designed & Developed by Blake Zajac — https://github.com/BlakeZajac/`
+);
 
 document.addEventListener("DOMContentLoaded", (event) => {
   stickyMobileHeader(980);
@@ -10,12 +10,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   let headerEmail = document.createElement("div");
   headerEmail.classList.add("header__email");
-  let headerEmailHTML = '<div class="header__email__content"><a href="mailto:contact@blakezajac.com">contact@blakezajac.com</a></div>';
+  let headerEmailHTML =
+    '<div class="header__email__content"><a href="mailto:contact@blakezajac.com">contact@blakezajac.com</a></div>';
   headerEmail.innerHTML = headerEmailHTML;
 
   desktopHeader.append(headerEmail);
 
   // to do - rewrite with vanilla JS
+  $(".hero__content h2 span").mouseenter(function () {
+    $(".hero__content h2").css("color", "var(--black-20");
+    $(".hero__content h2").css("transition", "all 0.2s ease-in-out");
+  });
+  $(".hero__content h2 span").mouseleave(function () {
+    $(".hero__content h2").css("color", "var(--black-100)");
+    $(".hero__content h2").css("transition", "all 0.2s ease-in-out");
+  });
+
   // const heroHoverItem = document.querySelectorAll(".hero__content h2 span");
   // const heroHeading = document.querySelectorAll(".hero__content h2");
 
@@ -27,13 +37,4 @@ document.addEventListener("DOMContentLoaded", (event) => {
   //   heroHeading.style.color = "var(--black-100)";
   //   heroHeading.style.transition = "all 0.2s ease-in-out";
   // });
-
-  $('.hero__content h2 span').mouseenter(function(){
-     $('.hero__content h2').css("color", "var(--black-20");
-     $('.hero__content h2').css("transition", "all 0.2s ease-in-out");
-    });
-  $('.hero__content h2 span').mouseleave(function(){
-     $('.hero__content h2').css("color", "var(--black-100)");
-      $('.hero__content h2').css("transition", "all 0.2s ease-in-out");
-    });
 });
