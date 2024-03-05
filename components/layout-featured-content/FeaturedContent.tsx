3ea2da6a627640352b8c78dfc4c121a67ff3fe85
BlakeZajac <contact@blakezajac.com>
2024-03-06 09:31:09 +1100

    Render statistics above or below media depending on breakpoint

--- components/layout-featured-content/FeaturedContent.tsx
+++ components/layout-featured-content/FeaturedContent.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Image, { StaticImageData } from "next/image";
 
 import Section from "../layout-section/Section";
@@ -47,6 +50,45 @@ const FeaturedContent: React.FC<FeaturedContentProps> = ({
     mediaPosition === "left" ? "l-grid--row-reverse-at-lg" : ""
   }`;
 
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    setWindowWidth(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const isLargeScreen = windowWidth >= 1024;
+
+  const renderStatistics = () => {
+    return (
+      <div className="featured-content__unit featured-content__unit--statistic">
+        {statisticNumber01 && (
+          <Statistic
+            number={statisticNumber01}
+            numberSuffix={statisticNumberSuffix01}
+            content={statisticContent01}
+          />
+        )}
+        {statisticNumber02 && (
+          <Statistic
+            number={statisticNumber02}
+            numberSuffix={statisticNumberSuffix02}
+            content={statisticContent02}
+          />
+        )}
+      </div>
+    );
+  };
+
   return (
     <Section className={`featured-content ${componentClass}`}>
       <Container className="featured-content__container">
@@ -54,51 +96,30 @@ const FeaturedContent: React.FC<FeaturedContentProps> = ({
         <div className={`featured-content__grid l-grid ${gridClass}`}>
           <div className="featured-content__cell featured-content__cell--content l-grid__cell l-grid__cell--50-at-lg l-grid__cell--25-at-2xl">
             <div className="featured-content__unit featured-content__unit--copy copy">
-              {content01 || content02 ? (
-                <>
-                  {content01 ? <p>{content01}</p> : ""}
-                  {content02 ? <p>{content02}</p> : ""}
-                </>
-              ) : (
-                ""
-              )}
-              {buttonHref ? (
+              {content01 ||
+                (content02 && (
+                  <>
+                    {content01 && <p>{content01}</p>}
+                    {content02 && <p>{content02}</p>}
+                  </>
+                ))}
+              {buttonHref && (
                 <Button buttonHref={buttonHref} buttonLabel={buttonLabel} />
-              ) : (
-                ""
               )}
             </div>
-            <div className="featured-content__unit featured-content__unit--statistic">
-              {statisticNumber01 ? (
-                <Statistic
-                  number={statisticNumber01}
-                  numberSuffix={statisticNumberSuffix01}
-                  content={statisticContent01}
-                />
-              ) : (
-                ""
-              )}
-              {statisticNumber02 ? (
-                <Statistic
-                  number={statisticNumber02}
-                  numberSuffix={statisticNumberSuffix02}
-                  content={statisticContent02}
-                />
-              ) : (
-                ""
-              )}
-            </div>
+            {isLargeScreen && renderStatistics()}
           </div>
           <div className="featured-content__cell featured-content__cell--media l-grid__cell l-grid__cell--50-at-lg l-grid__cell--75-at-2xl">
-            {image ? (
-              <Image
-                src={image}
-                alt={imageAlt}
-                className="featured-content__media u-rounded"
-              />
-            ) : (
-              ""
+            {image && (
+              <div className="featured-content__unit featured-content__unit--media">
+                <Image
+                  src={image}
+                  alt={imageAlt}
+                  className="featured-content__media u-rounded"
+                />
+              </div>
             )}
+            {!isLargeScreen && renderStatistics()}
           </div>
         </div>
       </Container>
