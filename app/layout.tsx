8ca9a58b04d0ed3c4d0a0bff0d36a49e985dfcc9
BlakeZajac <contact@blakezajac.com>
2024-03-06 00:04:51 +1100

    Load brand fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/lec4mqw.css" />
+      </head>
       <body>
         {/* <SkipToMain /> */}
         <SiteHeader />
