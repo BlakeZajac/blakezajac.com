cd35bab762abab37ea7073e1890f3772d48cc9dd
BlakeZajac <contact@blakezajac.com>
2024-03-06 01:10:26 +1100

    Hero markup and props

---
+++ components/block-badge/Badge.tsx
@@ -0,0 +1,13 @@
+interface BadgeProps {
+  label: string;
+}
+
+const Badge: React.FC<BadgeProps> = ({ label }) => {
+  return (
+    <div className="badge">
+      <span className="badge__label">{label}</span>
+    </div>
+  );
+};
+
+export default Badge;
--- components/layout-hero/Hero.tsx
+++ components/layout-hero/Hero.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+"use client";
 
-const Hero = () => {
-  return <div>Hero</div>;
+import { twMerge } from "tailwind-merge";
+
+import Container from "../layout-container/Container";
+import Badge from "../block-badge/Badge";
+import Image from "next/image";
+
+// @todo - Load from Sanity CMS
+interface HeroProps {
+  className?: string;
+  badge?: string;
+  title?: string;
+  description01?: string;
+  description02?: string;
+  image?: string;
+  imageAlt?: string;
+}
+
+const Hero: React.FC<HeroProps> = ({
+  className,
+  badge,
+  title,
+  description01,
+  description02,
+  image,
+  imageAlt,
+}) => {
+  return (
+    <div className={twMerge(className, `hero`)}>
+      <Container className="hero__container">
+        {badge ? <Badge label={badge} /> : ""}
+        {title ? <h1 className="hero__title">{title}</h1> : ""}
+        {description01 || description02 ? (
+          <div className="hero__content">
+            {description01 ? <p>{description01}</p> : ""}
+            {description02 ? <p>{description02}</p> : ""}
+          </div>
+        ) : (
+          ""
+        )}
+        {image ? (
+          <Image className="hero__media" src={image} alt={imageAlt} />
+        ) : (
+          ""
+        )}
+      </Container>
+    </div>
+  );
 };
 
 export default Hero;
