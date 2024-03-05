f346cf51bb16b00704228b2d570d80ae3bba6188
BlakeZajac <contact@blakezajac.com>
2024-03-05 21:52:03 +1100

    Remove some unrequired boilerplate files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
 import "../build/css/style.min.css";
 import "../build/js/main.min.js";
 
 import SiteHeader from "@/components/layout-site-header/SiteHeader";
-
-const inter = Inter({ subsets: ["latin"] });
+import SiteFooter from "@/components/layout-site-footer/SiteFooter";
 
 export const metadata: Metadata = {
   title: "Blake Zajac",
@@ -20,9 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <SiteHeader />
         {children}
+        <SiteFooter />
       </body>
     </html>
   );
