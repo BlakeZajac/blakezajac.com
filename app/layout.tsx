83bb6cd5ff5d792be5ed6751ea528cca9379a5fe
BlakeZajac <contact@blakezajac.com>
2024-03-05 21:59:32 +1100

    Render components globally and on home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "../build/js/main.min.js";
 import SiteHeader from "@/components/layout-site-header/SiteHeader";
 import SiteFooter from "@/components/layout-site-footer/SiteFooter";
 import SkipToMain from "@/components/layout-skip-to-main/SkipToMain";
+import SiteOffcanvas from "@/components/layout-site-offcanvas/SiteOffcanvas";
 
 export const metadata: Metadata = {
   title: "Blake Zajac",
@@ -24,6 +25,7 @@ export default function RootLayout({
         <SiteHeader />
         {children}
         <SiteFooter />
+        <SiteOffcanvas />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import BackgroundCardCollection from "@/components/layout-background-card-collection/BackgroundCardCollection";
+import CalloutContent from "@/components/layout-callout-content/CalloutContent";
+import FeaturedContent from "@/components/layout-featured-content/FeaturedContent";
+import FeaturedProjects from "@/components/layout-featured-projects/FeaturedProjects";
+import Hero from "@/components/layout-hero/Hero";
+
 export default function Home() {
   return (
     <main className="site-main" id="main">
-      <div>
-        <div></div>
-      </div>
+      <Hero />
+      <CalloutContent />
+      <FeaturedProjects />
+      <FeaturedContent />
+      <BackgroundCardCollection />
     </main>
   );
 }
