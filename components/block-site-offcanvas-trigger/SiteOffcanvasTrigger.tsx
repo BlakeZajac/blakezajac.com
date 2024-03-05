8fae59d92f93d92a0bc86c94334f5601627a95ac
BlakeZajac <contact@blakezajac.com>
2024-03-06 00:29:03 +1100

    Offcanvas trigger styles + active class logic

--- components/block-site-offcanvas-trigger/SiteOffcanvasTrigger.tsx
+++ components/block-site-offcanvas-trigger/SiteOffcanvasTrigger.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 
 const SiteOffcanvasTrigger = () => {
-  return <div>SiteOffcanvasTrigger</div>;
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleActive = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <button
+      className={`site-offcanvas-trigger ${
+        isActive ? "site-offcanvas-trigger--is-active" : ""
+      }`}
+      onClick={toggleActive}
+    >
+      <span className="site-offcanvas-trigger__icon"></span>
+      <span className="site-offcanvas-trigger__label u-visually-hidden">
+        Menu
+      </span>
+    </button>
+  );
 };
 
 export default SiteOffcanvasTrigger;
