e7c5c01ece5cab2fdc0d7f702eaf7371af7fd36d
BlakeZajac <contact@blakezajac.com>
2024-03-05 23:36:28 +1100

    Primary menu markup

--- components/block-primary-menu/PrimaryMenu.tsx
+++ components/block-primary-menu/PrimaryMenu.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+const menuItems = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Work",
+    href: "/work",
+  },
+  {
+    label: "Expertise",
+    href: "/expertise",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
+
+interface MenuItemProps {
+  label: string;
+  href: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ label, href }) => {
+  return (
+    <li className="primary-menu__item">
+      <a href={href} className="primary-menu__link" itemProp="url">
+        <span className="primary-menu__label">{label}</span>
+        <meta content={label} itemProp="name" />
+      </a>
+    </li>
+  );
+};
 
 const PrimaryMenu = () => {
-  return <div>PrimaryMenu</div>;
+  return (
+    <nav className="primary-menu">
+      <ul
+        itemType="http://schema.org/SiteNavigationElement"
+        itemScope={true}
+        className="primary-menu__list"
+      >
+        {menuItems.map((menuItem, index) => (
+          <MenuItem key={index} label={menuItem.label} href={menuItem.href} />
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default PrimaryMenu;
