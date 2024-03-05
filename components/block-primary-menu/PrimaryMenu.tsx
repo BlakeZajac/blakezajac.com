d556831890d30b772a9bc9fd2761ab36da942939
BlakeZajac <contact@blakezajac.com>
2024-03-06 00:09:27 +1100

    Add logic to add active class to active page in primary menu

--- components/block-primary-menu/PrimaryMenu.tsx
+++ components/block-primary-menu/PrimaryMenu.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 // @todo - Move menuItems array to Sanity CMS
 const menuItems = [
   {
@@ -28,9 +32,22 @@ interface MenuItemProps {
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ label, href }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const currentPath = window.location.pathname;
+    setIsActive(currentPath === href);
+  }, [href]);
+
   return (
     <li className="primary-menu__item">
-      <a href={href} className="primary-menu__link" itemProp="url">
+      <a
+        href={href}
+        className={`primary-menu__link ${
+          isActive ? "primary-menu__link--active" : null
+        }`}
+        itemProp="url"
+      >
         <span className="primary-menu__label">{label}</span>
         <meta content={label} itemProp="name" />
       </a>
