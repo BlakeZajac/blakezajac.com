93dc8efbdb6e5cb68422588c665d84c0f1e99604
BlakeZajac <contact@blakezajac.com>
2024-03-06 20:29:27 +1100

    Follow the mouse on button hover

--- components/block-button/Button.tsx
+++ components/block-button/Button.tsx
@@ -1,5 +1,8 @@
+import React, { useState } from "react";
 import { twMerge } from "tailwind-merge";
+
 import { MdArrowForward } from "react-icons/md";
+import { FaRegSmile } from "react-icons/fa";
 
 interface ButtonProps {
   buttonClassname?: string;
@@ -14,15 +17,29 @@ const Button: React.FC<ButtonProps> = ({
   buttonTarget = "_self",
   buttonLabel,
 }) => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    setMousePosition({ x: event.clientX, y: event.clientY });
+  };
+
   return (
     <a
       href={buttonHref}
       className={twMerge(buttonClassname, `btn`)}
       target={buttonTarget}
+      onMouseMove={handleMouseMove}
     >
       <span className="btn__label">{buttonLabel}</span>
       <div className="btn__accessory">
         <MdArrowForward className="btn__icon btn__icon--arrow" />
+        <FaRegSmile
+          style={{
+            left: `${mousePosition.x - 8}px`,
+            top: `${mousePosition.y - 8}px`,
+          }}
+          className="btn__icon btn__icon--smile"
+        />
       </div>
     </a>
   );
