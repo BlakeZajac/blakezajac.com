37cd29ec29bbd29d900eeb56047cc952c00c4fcc
BlakeZajac <contact@blakezajac.com>
2024-03-07 20:35:45 +1100

    Site legal markup

--- components/block-site-legal/SiteLegal.tsx
+++ components/block-site-legal/SiteLegal.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Image from "next/image";
+
+import Acknowledgement from "../layout-acknowledgement/Acknowledgement";
+import logo from "../../src/img/logo/logo.svg";
 
 const SiteLegal = () => {
-  return <div>SiteLegal</div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="site-legal">
+      <div className="site-legal__unit site-legal__unit--logo">
+        <Image src={logo} alt="Blake Zajac logo" className="site-legal__logo" />
+      </div>
+      <div className="site-legal__unit site-legal__unit--copyright">
+        © {currentYear} Blake Zajac
+      </div>
+      <div className="site-legal__unit site-legal__unit--acknowledgement">
+        <Acknowledgement />
+      </div>
+    </div>
+  );
 };
 
 export default SiteLegal;
