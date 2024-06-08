eae596ac63e3d8e7246efd2d8f5e423e7d4a00db
Blake Zajac <contact@blakezajac.com>
2024-06-08 17:43:28 +1000

    Add a send-email utility

---
+++ components/block-contact-form/ContactForm.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+
+import { useForm } from "react-hook-form";
+
+export type FormData = {
+    name: string;
+    company: string;
+    phone: string;
+    email: string;
+    message: string;
+    leadLocation: string;
+};
+
+const ContactForm = () => {
+    return <div>ContactForm</div>;
+};
+
+export default ContactForm;
---
+++ utilities/send-email.ts
@@ -0,0 +1,6 @@
+import { FormData } from "@/components/block-contact-form/ContactForm";
+
+export function sendEmail(data: FormData) {
+    // @todo - send email
+    console.log(data);
+}
