9e7ff995a9717a42c6a4c53caa552d96df640375
Blake Zajac <contact@blakezajac.com>
2024-06-08 17:39:33 +1000

    npm i react-hook-form

--- components/layout-form/Form.tsx
+++ components/layout-form/Form.tsx
@@ -24,36 +24,42 @@ const Form: React.FC<FormProps> = ({ phoneNumber, email, title, description, des
         <Section>
             <Container className="l-container--max-width_1440">
                 <div className="form">
-                    {(sanitisedPhoneNumber || email) && (
-                        <div className="form-links">
-                            {sanitisedPhoneNumber && (
-                                <a
-                                    href={`tel:${sanitisedPhoneNumber}`}
-                                    className="form-links__item"
-                                >
-                                    {phoneNumber}
-                                </a>
-                            )}
-                            {email && (
-                                <a href={`mailto:${email}`} className="form-links__item">
-                                    {email}
-                                </a>
-                            )}
-                        </div>
-                    )}
-                    <h1 className="form__title">{title}</h1>
-                    {(description || descriptionTwo) && (
-                        <div className="form-description">
-                            {description && (
-                                <div className="form-description__item">{description}</div>
-                            )}
-                            {descriptionTwo && (
-                                <div className="form-description__item form-description__item--alt">
-                                    {descriptionTwo}
-                                </div>
-                            )}
-                        </div>
-                    )}
+                    <div className="form__unit form__unit--content">
+                        {(sanitisedPhoneNumber || email) && (
+                            <div className="form-links">
+                                {sanitisedPhoneNumber && (
+                                    <a
+                                        href={`tel:${sanitisedPhoneNumber}`}
+                                        className="form-links__item underlink"
+                                    >
+                                        {phoneNumber}
+                                    </a>
+                                )}
+                                {email && (
+                                    <a
+                                        href={`mailto:${email}`}
+                                        className="form-links__item underlink"
+                                    >
+                                        {email}
+                                    </a>
+                                )}
+                            </div>
+                        )}
+                        <h1 className="form__title">{title}</h1>
+                        {(description || descriptionTwo) && (
+                            <div className="form-description">
+                                {description && (
+                                    <div className="form-description__item">{description}</div>
+                                )}
+                                {descriptionTwo && (
+                                    <div className="form-description__item form-description__item--alt">
+                                        {descriptionTwo}
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                    </div>
+                    <div className="form__unit form__unit--form"></div>
                 </div>
             </Container>
         </Section>
