c2ec95580ebdf41538ac6a80b263980b7b8be03f
Blake Zajac <contact@blakezajac.com>
2024-06-15 18:42:00 +1000

    Set up IconGrid props

--- components/layout-icon-grid/IconGrid.tsx
+++ components/layout-icon-grid/IconGrid.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { StaticImageData } from "next/image";
 
-const IconGrid = () => {
-  return <div>IconGrid</div>;
+interface IconGridItem {
+    icon?: StaticImageData | string;
+    title?: string;
+    description?: string;
+    link?: string;
+}
+
+interface IconGridProps {
+    title?: string;
+    description?: string;
+    columns: 2 | 3 | 4;
+    items: IconGridItem[];
+}
+
+const IconGrid: React.FC<IconGridProps> = ({ title, description, columns, items }) => {
+    return <div>IconGrid</div>;
 };
 
 export default IconGrid;
