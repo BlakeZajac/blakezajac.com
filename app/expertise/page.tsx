f0374ce66c41e63aa63cfcd99e08f5785752899e
Blake Zajac <contact@blakezajac.com>
2024-06-15 18:36:39 +1000

    Add hero to expertise page

--- app/expertise/page.tsx
+++ app/expertise/page.tsx
@@ -1,5 +1,21 @@
+import Hero from "@/components/layout-hero/Hero";
+
+import heroImage from "../../src/img/headshots/headshots-1920x1080-01.webp";
+import Waypoint from "@/components/block-waypoint/Waypoint";
+
 const ExpertisePage = () => {
-    return <></>;
+    return (
+        <>
+            <Waypoint activeClass="theme-orange">
+                <Hero
+                    className="u-bg-orange--light"
+                    badge="UX Design"
+                    title="Strategic foundations lead to better outcomes"
+                    image={heroImage}
+                />
+            </Waypoint>
+        </>
+    );
 };
 
 export default ExpertisePage;
