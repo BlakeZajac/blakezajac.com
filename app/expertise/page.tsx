9cd78e5671a6f42875d58c68e4dcc69899b971f4
Blake Zajac <contact@blakezajac.com>
2024-06-20 20:30:47 +1000

    Media content markup

--- app/expertise/page.tsx
+++ app/expertise/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/layout-hero/Hero";
 import Waypoint from "@/components/block-waypoint/Waypoint";
 import IconGrid from "@/components/layout-icon-grid/IconGrid";
+import MediaContent from "@/components/layout-media-content/MediaContent";
 
 import heroImage from "../../src/img/headshots/headshots-1920x1080-01.webp";
 import expertiseItems from "../../src/data/expertise";
@@ -22,6 +23,27 @@ const ExpertisePage = () => {
                 description="Create a crystal clear vision for your digital product and put your customer at the heart of the solution."
                 columns={4}
                 items={expertiseItems}
+            />
+
+            <MediaContent
+                media={heroImage}
+                title="Reimaged user experiences"
+                content="We work with forward-thinking organisations to reimagine what good looks like. From simplifying complex user journeys to improving onboarding, we've helped solve a wide array of business problems through a strategic approach to UX."
+                linkUrl="#"
+                linkTitle="Learn more"
+            />
+
+            <MediaContent
+                mediaPosition="right"
+                media={heroImage}
+                title="Optimised for conversion"
+                content="Utilising an analytical approach combined with our best practice methodologies, we convert interest into action. By focusing on streamlined pathways and clear calls to action, we capture user attention and drive them towards desired outcomes, be it sales, sign-ups, or engagement."
+            />
+
+            <MediaContent
+                media={heroImage}
+                title="Set for the future"
+                content="Our digital strategies are designed to not only meet current demands but also anticipate future trends and challenges. We focus on scalability, flexibility, and innovation to keep your business at the forefront, ready to embrace new opportunities and technologies as they arise."
             />
         </>
     );
--- components/layout-media-content/MediaContent.tsx
+++ components/layout-media-content/MediaContent.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+"use client";
 
-const MediaContent = () => {
-    return <div>MediaContent</div>;
+import { twMerge } from "tailwind-merge";
+import Image, { StaticImageData } from "next/image";
+
+import Section from "../layout-section/Section";
+import Container from "../layout-container/Container";
+import Button from "../block-button/Button";
+
+interface MediaContentProps {
+    mediaPosition?: "left" | "right";
+    media: StaticImageData | string;
+    mediaAlt?: string;
+    title?: string;
+    content?: string;
+    linkUrl?: string;
+    linkTitle?: string;
+}
+
+const MediaContent: React.FC<MediaContentProps> = ({
+    mediaPosition = "left",
+    media,
+    mediaAlt = "",
+    title,
+    content,
+    linkUrl,
+    linkTitle,
+}) => {
+    return (
+        <Section className={twMerge(`media-content media-content--media-${mediaPosition}`)}>
+            <Container>
+                <div className="media-content__grid l-grid l-grid--vertical-gap">
+                    <div className="l-grid__cell l-grid__cell--50-at-lg">
+                        <Image src={media} alt={mediaAlt} className="media-content__image" />
+                    </div>
+                    <div className="l-grid__cell l-grid__cell--50-at-lg">
+                        <div className="media-content__copy copy">
+                            {title && <h2 className="media-content__title">{title}</h2>}
+                            {content && <p>{content}</p>}
+                            {linkUrl && linkTitle && (
+                                <Button
+                                    buttonHref={linkUrl}
+                                    buttonLabel={linkTitle}
+                                    buttonClassname="button-group__button"
+                                />
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </Section>
+    );
 };
 
 export default MediaContent;
