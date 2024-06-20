c2c3eb6d44fccce89b626dc630e8272860e96699
Blake Zajac <contact@blakezajac.com>
2024-06-20 20:50:58 +1000

    WIP background card layout

--- components/block-background-card/BackgroundCard.tsx
+++ components/block-background-card/BackgroundCard.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
 
-const BackgroundCard = () => {
-  return <div>BackgroundCard</div>;
+import { StaticImageData } from "next/image";
+import { twMerge } from "tailwind-merge";
+
+/** @todo The following logic needs to be set up
+ *
+ * A project card will render a orange badge
+ * A news card will render a yellow badge
+ * A custom label will render a glass badge
+ */
+
+export interface BackgroundCardProps {
+    className?: string;
+    type: "project" | "post" | "custom";
+    media: StaticImageData | string;
+}
+
+const BackgroundCard: React.FC<BackgroundCardProps> = ({ className }) => {
+    return <div className={(twMerge(`background-card`), className)}>BackgroundCard</div>;
 };
 
 export default BackgroundCard;
--- components/layout-background-card-collection/BackgroundCardCollection.tsx
+++ components/layout-background-card-collection/BackgroundCardCollection.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import BackgroundCard, { BackgroundCardProps } from "../block-background-card/BackgroundCard";
 
-const BackgroundCardCollection = () => {
-  return <div>BackgroundCardCollection</div>;
+import Section from "../layout-section/Section";
+import Container from "../layout-container/Container";
+
+interface BackgroundCardCollectionProps {
+    title?: string;
+    items: BackgroundCardProps[];
+}
+
+const BackgroundCardCollection: React.FC<BackgroundCardCollectionProps> = ({ title, items }) => {
+    return (
+        <Section>
+            <Container>
+                {items.map((item, index) => (
+                    <BackgroundCard key={index} {...item} />
+                ))}
+            </Container>
+        </Section>
+    );
 };
 
 export default BackgroundCardCollection;
