8a811c9d05caade58dd9861324c2579782a4be07
Blake Zajac <contact@blakezajac.com>
2024-06-20 19:59:17 +1000

    Add icon and dummy link to expertise data

--- src/data/expertise.js
+++ src/data/expertise.js
@@ -7,25 +7,28 @@
  * @property    {string}                    [linkTitle]     The title of the link.
  */
 
+import icon from "../img/icon/icon-expertise.svg";
+
 // @todo - Sanity
 const expertiseItems = [
     {
-        icon: '',
+        icon: icon,
         title: 'Insight rich',
         description: 'We support a wide range of analytical and insight gathering activities, designed to understand audience behaviour and to help inform our strategic recommendations.',
+        link: '#',
+        linkTitle: 'Learn more'
     },
     {
-        icon: '',
+        icon: icon,
         title: 'Structured foundations',
         description: 'Once the problem has been defined, we validate the proposed solutions in the form of user journeys, IA, wireframing and prototyping.',
     },
     {
-        icon: '',
+        icon: icon,
         title: 'Design thinking',
         description: 'We combine audience insights with our strategic expertise to fuel intensive periods of conceptual thinking, creating solutions that keep your brand moving forward.',
     },
     {
-        icon: '',
         title: 'Strategic execution',
         description: 'Our strategic solutions unlock opportunities and solve business problems. We create the best possible experience and the right conditions to deliver.',
     },
