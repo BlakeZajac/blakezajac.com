6b655ded333185c586af48ea6b20e1ecfd2ea5af
Blake Zajac <contact@blakezajac.com>
2024-06-22 20:52:32 +1000

    Work schema

--- sanity/schemas/work-schema.ts
+++ sanity/schemas/work-schema.ts
@@ -9,5 +9,27 @@ export const workType = defineType({
             name: "title",
             type: "string",
         }),
+        defineField({
+            name: "slug",
+            type: "slug",
+            options: { source: "title" },
+        }),
+        defineField({
+            name: "featuredImage",
+            title: "Featured Image",
+            type: "image",
+            options: { hotspot: true },
+            fields: [
+                {
+                    name: "alt",
+                    type: "string",
+                },
+            ],
+        }),
+        defineField({
+            name: "content",
+            type: "array",
+            of: [{ type: "block" }],
+        }),
     ],
 });
