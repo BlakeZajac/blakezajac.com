9aa6911f0f3f73833960d51b8bf458c664f8c777
Blake Zajac <contact@blakezajac.com>
2024-06-22 14:57:22 +1000

    Stick project card title to bottom

--- components/block-project-card/ProjectCard.tsx
+++ components/block-project-card/ProjectCard.tsx
@@ -5,15 +5,19 @@ import { twMerge } from "tailwind-merge";
 
 export interface ProjectCardProps {
     className?: string;
+    buttonHref: string;
     media: StaticImageData | string;
     title: string;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ className, media, title }) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({ className, buttonHref, media, title }) => {
     return (
         <div className={twMerge(`project-card`, className)}>
+            <a href={buttonHref} className="project-card__link"></a>
             <div className="project-card__media">
                 <Image src={media} alt="" className="project-card__image" />
+            </div>
+            <div className="project-card__content">
                 <div className="project-card__title">{title}</div>
             </div>
         </div>
