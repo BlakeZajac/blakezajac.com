e86d0f90faf6d5153cbbb7d4ead1671c29ae14a9
Blake Zajac <blake@rb.com.au>
2024-09-01 18:17:23 +1000

    Increate lerp

--- providers/LenisProvider.tsx
+++ providers/LenisProvider.tsx
@@ -3,15 +3,15 @@
 import { ReactLenis } from "@studio-freight/react-lenis";
 
 interface LenisProviderProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 const LenisProvider: React.FC<LenisProviderProps> = ({ children }) => {
-  return (
-    <ReactLenis root options={{ lerp: 0.075, duration: 1.5 }}>
-      {children}
-    </ReactLenis>
-  );
+    return (
+        <ReactLenis root options={{ lerp: 0.085, duration: 1.5 }}>
+            {children}
+        </ReactLenis>
+    );
 };
 
 export default LenisProvider;
