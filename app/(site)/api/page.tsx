97a814c1f66347b4db8535f4e42963fbe8d2684c
Blake Zajac <contact@blakezajac.com>
2024-09-03 20:48:29 +1000

    Next api page

--- app/(site)/api/page.tsx
+++ app/(site)/api/page.tsx
@@ -5,16 +5,20 @@ import { ReadingList } from "@/types/reading-list";
 
 import { useReadingList } from "@/hooks/use-reading-list";
 import { useWork } from "@/hooks/use-work";
+import Section from "@/components/layout-section/Section";
+import Container from "@/components/layout-container/Container";
 
 const ApiPage = () => {
     const work: Work[] = useWork();
     const readingList: ReadingList[] = useReadingList();
 
     return (
-        <div>
-            <pre>{JSON.stringify(work, null, 2)}</pre>
-            <pre>{JSON.stringify(readingList, null, 2)}</pre>
-        </div>
+        <Section>
+            <Container>
+                <pre>{JSON.stringify(work, null, 2)}</pre>
+                <pre>{JSON.stringify(readingList, null, 2)}</pre>
+            </Container>
+        </Section>
     );
 };
 
