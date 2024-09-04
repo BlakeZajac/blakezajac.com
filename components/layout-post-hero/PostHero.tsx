a17e8c31d77763007492f98b07f07cb0ea1b9ad3
Blake Zajac <contact@blakezajac.com>
2024-09-04 19:48:32 +1000

    Remove twMerge from post hero

--- components/layout-post-hero/PostHero.tsx
+++ components/layout-post-hero/PostHero.tsx
@@ -1,16 +1,13 @@
 import Image from "next/image";
-import { twMerge } from "tailwind-merge";
 
 import { Work } from "@/types/work";
 
 import Section from "@/components/layout-section/Section";
 import Container from "@/components/layout-container/Container";
 
-interface PostHeroProps extends Pick<Work, "title" | "shortDescription" | "shortDescriptionAlt" | "liveLink" | "featuredImage"> {
-    className?: string;
-}
+interface PostHeroProps extends Pick<Work, "title" | "shortDescription" | "shortDescriptionAlt" | "liveLink" | "featuredImage"> {}
 
-const PostHero: React.FC<PostHeroProps> = ({ className, title, shortDescription, shortDescriptionAlt, liveLink, featuredImage }) => {
+const PostHero: React.FC<PostHeroProps> = ({ title, shortDescription, shortDescriptionAlt, liveLink, featuredImage }) => {
     if (!title) {
         return;
     }
@@ -24,7 +21,7 @@ const PostHero: React.FC<PostHeroProps> = ({ className, title, shortDescription,
     return (
         <Section>
             <Container>
-                <div className={`post-hero ${twMerge(className)}`}>
+                <div className="post-hero">
                     <div className="post-hero__unit post-hero__unit--01">
                         <div className="post-hero__accessory">Accessory</div>
                         <h1 className="post-hero__title">{title}</h1>
