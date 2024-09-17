e3b1efc8eb16cc05ece61cf99285d1eb0bd99d60
Blake Zajac <contact@blakezajac.com>
2024-09-17 12:55:44 +1000

    Remove isRounded from post hero

--- components/layout-post-hero/PostHero.tsx
+++ components/layout-post-hero/PostHero.tsx
@@ -7,7 +7,7 @@ import RevealImage from "../block-reveal-image/RevealImage";
 interface PostHeroProps extends Pick<Work, "title" | "featuredImage"> {}
 
 const PostHero: React.FC<PostHeroProps> = ({ title, featuredImage }) => {
-    return <RevealImage image={featuredImage} imageAlt={title}></RevealImage>;
+    return <RevealImage image={featuredImage} imageAlt={title} isRounded={false}></RevealImage>;
 };
 
 export default PostHero;
