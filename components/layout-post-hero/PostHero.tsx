4526c0dd294359f3e5a091b3033cd134360e063f
Blake Zajac <contact@blakezajac.com>
2024-10-15 20:05:28 +1100

    Add url to reveal image prop

--- components/layout-post-hero/PostHero.tsx
+++ components/layout-post-hero/PostHero.tsx
@@ -7,7 +7,13 @@ import RevealImage from "../block-reveal-image/RevealImage";
 interface PostHeroProps extends Pick<Work, "title" | "featuredImage"> {}
 
 const PostHero: React.FC<PostHeroProps> = ({ title, featuredImage }) => {
-    return <RevealImage image={featuredImage} imageAlt={title} isRounded={false}></RevealImage>;
+  return (
+    <RevealImage
+      image={featuredImage.url}
+      imageAlt={title}
+      isRounded={false}
+    ></RevealImage>
+  );
 };
 
 export default PostHero;
