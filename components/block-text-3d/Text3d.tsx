e2d900735c46bc8f3e68003ba5619ce6ebd08f1d
Blake Zajac <contact@blakezajac.com>
2024-10-20 19:00:45 +1100

    Update height of the footer cta based on the index of the text3d count

--- components/block-text-3d/Text3d.tsx
+++ components/block-text-3d/Text3d.tsx
@@ -1,15 +1,34 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 interface Text3dProps {
   primary: string;
   secondary: string;
 }
 
+let instanceCount = 0;
+
 const Text3d: React.FC<Text3dProps> = ({ primary, secondary }) => {
   const textPrimary = useRef(null);
   const textSecondary = useRef(null);
+
+  useEffect(() => {
+    instanceCount++;
+
+    document.documentElement.style.setProperty(
+      "--text-3d-count",
+      instanceCount.toString()
+    );
+
+    return () => {
+      instanceCount--;
+      document.documentElement.style.setProperty(
+        "--text-3d-count",
+        instanceCount.toString()
+      );
+    };
+  }, []);
 
   return (
     <div className="text-3d">
